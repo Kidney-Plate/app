7ffb8ced6f298374ce5b3435031c0bd7ea29e340
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2023-10-22 16:26:44 -0400

    Added native search bar component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import 'expo-dev-client';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import "expo-dev-client";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SearchBar } from "@rneui/themed";
 
 export default function App() {
+  type SearchBarComponentProps = {};
+
+  const SwitchComponent: React.FunctionComponent<
+    SearchBarComponentProps
+  > = () => {
+    const [search, setSearch] = useState("");
+
+    const updateSearch = (search: string) => {
+      setSearch(search);
+    };
+
+    return (
+      <View className="mt-11">
+        <SearchBar
+          placeholder="Search for foods..."
+          onChangeText={updateSearch}
+          value={search}
+          className="p-3"
+          platform="ios"
+          returnKeyType="search"
+        />
+      </View>
+    );
+  };
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View className="bg-white">
+        <SwitchComponent />
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
