85e429efd6f7d83fe6c4fa439f9d04fdb22b99d0
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2023-11-06 21:43:10 -0500

    Upgraded NativeWind from v2 to v4

--- App.tsx
+++ App.tsx
@@ -1,43 +1,60 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, StyleSheet, Keyboard, TextInput } from "react-native";
 import "expo-dev-client";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SearchBar from "./components/SearchBar";
 import client from "./lib";
+import "./global.css"
 
 export default function App() {
-  const [data, setData] = useState<any>();
+  const [data, setData] = useState<any>(null);
 
   const fetchData = async (term: string) => {
-    try {
-      const response = await client.POST("/v1/foods/search", {
-        body: {
-          query: term,
-        },
-      });
+    if (term.length > 2) {
+      try {
+        const response = await client.POST("/v1/foods/search", {
+          body: {
+            query: term,
+          },
+        });
 
-      setData(response);
-    } catch (error) {
-      setData(error);
+        setData(response);
+      } catch (error) {
+        setData(error);
+      }
+    } else {
+      setData(null);
     }
   };
 
   return (
     <SafeAreaProvider>
-      <View className="bg-white">
-        <View className="mt-12">
-          <SearchBar onUpdate={fetchData} />
+      <View className="bg-red">
+        <View className="mt-14 mx-2">
+          <SearchBar onUpdate={fetchData} onCancel={() => setData(null)} />
         </View>
         {data && (
+          // <FlatList
+          //   data={data.data.foods!}
+          //   removeClippedSubviews={false}
+          //   contentContainerStyle={{ paddingBottom: 120 }}
+          //   renderItem={({ item }) => (
+          //     <View className="p-3 border-b">
+          //       <Text className={"text-lg font-semibold text-gray-700"}>
+          //         {item.description}
+          //       </Text>
+          //     </View>
+          //   )}
+          // />
           <FlatList
-            data={data.data.foods!}
+            data={data.data.foods}
+            onScrollBeginDrag={() => Keyboard.dismiss()}
             removeClippedSubviews={false}
+            contentContainerStyle={{ paddingBottom: 130}}
             renderItem={({ item }) => (
-              <View className={"p-3 border-b"}>
-                <Text className={"text-lg font-semibold text-gray-700"}>
-                  {item.description}
-                </Text>
+              <View className="p-4 border-b-[1px] border-b-gray-200 bg-white">
+                <Text style={styles.listItemText}>{item.description}</Text>
               </View>
             )}
           />
@@ -47,3 +64,24 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white", // Background color of the whole screen
+  },
+  listContainer: {
+    paddingBottom: 120,
+  },
+  listItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#dcdcdc",
+    backgroundColor: "white", // Background color of each list item
+  },
+  listItemText: {
+    fontSize: 18,
+    color: "black",
+    fontFamily: "System", // iOS system font
+  },
+});
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { View } from "react-native";
+import { TextInput, View, StyleSheet, TouchableOpacity } from "react-native";
 import { SearchBar as NativeSearchBar } from "@rneui/themed";
+import { Feather } from "@expo/vector-icons";
 
 interface SearchBarProps {
   onUpdate?(param?: string): void;
+  onCancel?(): void;
 }
 
 const SearchBar = (props: SearchBarProps) => {
@@ -18,15 +20,50 @@ const SearchBar = (props: SearchBarProps) => {
   };
 
   return (
-    <NativeSearchBar
-      placeholder="Search for foods..."
-      onChangeText={updateSearch}
-      value={search}
-      className="p-3"
-      platform="ios"
-      returnKeyType="search"
-    />
+    // <TextInput
+    //   placeholder="Search"
+    //   className="p-3 rounded-md bg-gray-100"
+    //   placeholderTextColor="#4b5563"
+    // />
+    <View style={styles.searchContainer}>
+      <Feather name="search" size={24} color="#4b5563" />
+      <TextInput
+        placeholder="Search"
+        className="flex-1 ml-1 p-0"
+        placeholderTextColor="#4b5563"
+      />
+      <TouchableOpacity>
+        <Feather name="x" size={24} color="#4b5563" />
+      </TouchableOpacity>
+    </View>
+    // <NativeSearchBar
+    //   placeholder="Search for foods..."
+    //   onChangeText={updateSearch}
+    //   onCancel={() => (props.onCancel) && props.onCancel()}
+    //   value={search}
+    //   className="p-3"
+    //   platform="ios"
+    //   returnKeyType="search"
+    //   showCancel={false}
+    // />
   );
 };
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#E5E7EB',
+    borderRadius: 999,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
+    marginLeft: 8,
+    padding: 0, // Remove any default padding
+  },
+});
 
 export default SearchBar;
