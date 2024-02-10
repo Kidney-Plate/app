ec0622afaf3a5a7917cde6fbce80f15843024aee
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2024-02-10 18:16:53 -0500

    Fixed dimming issues

--- App.tsx
+++ App.tsx
@@ -18,8 +18,11 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {
   BottomSheetModalProvider,
   BottomSheetModal,
+  BottomSheetBackdropProps,
+  BottomSheetBackdrop,
 } from "@gorhom/bottom-sheet";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   const [data, setData] = useState<any>(null);
@@ -96,14 +99,24 @@ export default function App() {
     fetchData(search);
   };
 
+  const BackdropElement = useCallback(
+    (backdropProps: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop
+        {...backdropProps}
+        opacity={0.7}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+      />
+    ),
+    []
+  );
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <BottomSheetModalProvider>
-          <View
-            style={[styles.container, onFood && { backgroundColor: "#4C4C4C" }]}
-          >
-            <View className="mt-14 mx-2">
+          <View className="flex-1 items-center">
+            <View className="mt-16 mx-2">
               <SearchBar
                 placeholder="Search for foods"
                 value={search}
@@ -111,37 +124,32 @@ export default function App() {
                 platform="ios"
                 returnKeyType="search"
                 showCancel={false}
-                dimmed={onFood}
-                placeholderTextColor={onFood ? "#303031" : undefined}
                 disabled={onFood}
               />
-            </View>
 
-            {!loading && !onFood && data && (
-              <FlatList
-                data={data.data.foods}
-                onScrollBeginDrag={() => Keyboard.dismiss()}
-                removeClippedSubviews={false}
-                contentContainerStyle={{ paddingBottom: 130 }}
-                renderItem={({ item }) => (
-                  <>
-                    <View className="ml-4 border-t-[0.75px] border-t-gray-200"></View>
-                    <Pressable
-                      className="p-4 bg-transparent active:bg-gray-200" // #00000016, #d1d5db
-                      onPress={() => onClickFood(item)}
-                    >
-                      <Text style={styles.listItemText}>
-                        {capitalizeFirstLetter(item.description)}
-                      </Text>
-                    </Pressable>
-                    {item == data.data.foods[data.data.foods.length - 1] && (
-                      <View className="ml-4 border-b-[0.75px] border-b-gray-200"></View>
-                    )}
-                  </>
-                )}
-              />
-            )}
-            <StatusBar style="auto" />
+              {!loading && !onFood && data && (
+                <FlatList
+                  data={data.data.foods}
+                  onScrollBeginDrag={() => Keyboard.dismiss()}
+                  removeClippedSubviews={false}
+                  contentContainerStyle={{ paddingBottom: 130 }}
+                  renderItem={({ item }) => (
+                    <>
+                      <View className="border-b-[0.75px] border-b-black/10"></View>
+                      <Pressable
+                        className="p-4 bg-transparent active:bg-gray-200" // #00000016, #d1d5db
+                        onPress={() => onClickFood(item)}
+                      >
+                        <Text style={styles.listItemText}>
+                          {capitalizeFirstLetter(item.description)}
+                        </Text>
+                      </Pressable>
+                    </>
+                  )}
+                />
+              )}
+              <StatusBar style="auto" />
+            </View>
           </View>
           <View>
             <BottomSheetModal
@@ -152,6 +160,7 @@ export default function App() {
                 setOnFood(false);
                 setDate(new Date());
               }}
+              backdropComponent={BackdropElement}
             >
               <View className="mt-10 mx-6">
                 {selectedFood.map((nutrient: any) => (
@@ -221,5 +230,25 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "black",
     fontFamily: "System", // iOS system font
+  },
+  searchSection: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: "#818181",
+    color: "#424242",
   },
 });
