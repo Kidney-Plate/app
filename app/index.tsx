38bfb1c47bbf1c01a1af6dfc52a69e2328cec141
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2024-02-19 19:53:22 -0500

    Added login illustration

--- app/index.tsx
+++ app/index.tsx
@@ -8,6 +8,9 @@ import {
   Keyboard,
   TextInput,
   Pressable,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
 } from "react-native";
 import "expo-dev-client";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -24,38 +27,37 @@ import {
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Device from "expo-device";
 import Button from "../components/Button";
+import Illustration from "../assets/LoginIllustration.svg";
 
 export default function Page() {
   const [email, setEmail] = useState("");
 
   return (
-    <View className="flex-1 justify-center mx-2">
-      <Text className="text-3xl font-black text-center mb-5">
-        Continue with Email
-      </Text>
-      <View>
-        <Text
-          className="block text-sm font-medium text-gray-900"
-        >
-          Email
-        </Text>
-        <View className="relative mt-2 rounded-md shadow-sm">
-          <TextInput
-            id="email"
-            className="rounded-md border-solid border-[1px] border-slate-200 py-3 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-            placeholder="example@example.com"
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-            autoCapitalize="none"
-            textContentType="emailAddress"
-            autoComplete="email"
-          />
+    <View className="mt-20 mx-2">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View>
+          <Illustration width="100%" height={150} className="mb-5" />
+          <Text className="text-3xl font-black text-center mb-3">
+            Continue with Email
+          </Text>
+          <Text className="text-center mb-6">Get personalized diet plans, exercise routine, and more.</Text>
+          <Text className="block text-sm font-medium text-gray-900">Email</Text>
+          <View className="relative mt-2 rounded-md shadow-sm">
+            <TextInput
+              id="email"
+              className="rounded-md border-solid border-[1px] border-slate-200 py-3 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+              placeholder="example@example.com"
+              onChangeText={(text) => setEmail(text)}
+              value={email}
+              autoCapitalize="none"
+              textContentType="emailAddress"
+              autoComplete="email"
+            />
+          </View>
+          <Button text="Log in" link="/home" />
         </View>
-      </View>
-      <Button text="Log in" link="/home" />
+      </TouchableWithoutFeedback>
     </View>
-    // <GestureHandlerRootView style={{ flex: 1 }}>
-    // </GestureHandlerRootView>
   );
 }
 
